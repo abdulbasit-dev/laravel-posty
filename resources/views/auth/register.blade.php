fc06d3aacea90f08e56e46818333873c5f62f10b
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-12-22 10:25:27 +0300

    create register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="flex justify-center">
-    <div class="bg-white p-6 w-8/12 rounded-lg">
+    <div class="bg-white p-6 w-5/12 rounded-lg">
       Posts
     </div>
   </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>posty</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <title>posty</title>
 </head>
-<body>
-    @yield('content')
+
+<body class="bg-gray-200">
+
+  <nav class="py-6 px-16 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Home</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Dashboard</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Post</a>
+      </li>
+    </ul>
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Abdulbasit</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Login</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Logout</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Register</a>
+      </li>
+    </ul>
+  </nav>
+
+  @yield('content')
 </body>
+
 </html>
